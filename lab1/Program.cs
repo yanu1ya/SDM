42e234a6d527204075aaaedda7b82d57e18b7474
thank1ess <vladonyaroshchuk@gmail.com>
2022-02-22 09:45:06 +0200

    created findRoots and inputCorrectNumber functions (interactive mode works)

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -5,34 +5,58 @@ class Program
     static void Main(string[] args)
     {
         double a, b, c;
+        Console.WriteLine("You've chosen interactive mode.\nInput your numbers, please.");
+        a = inputCorrectNumber("a");
+        if (a == 0) // exception
+        {
+            Console.WriteLine("Error: a can't be equal to 0. Try again.");
+            a = inputCorrectNumber("a");
+        }
 
-        Console.Write("a = ");
-        a = Convert.ToDouble(Console.ReadLine());
+        b = inputCorrectNumber("b");
 
-        Console.Write("b = ");
-        b = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("c = ");
-        c = Convert.ToDouble(Console.ReadLine());
+        c = inputCorrectNumber("c");
 
         Console.WriteLine($"({a})x^2 + ({b})x + ({c}) = 0");
-        double d = Math.Pow(b, 2) - 4 * a * c;
+
+        findRoots(a, b, c);
+
+    }
+    static void findRoots(double a, double b, double c)
+    {
+        double d = Math.Pow(b, 2) - 4 * a * c; // discriminant 
         if (d > 0)
         {
             double x1, x2;
             x1 = (-b + Math.Sqrt(d)) / (2 * a);
             x2 = (-b - Math.Sqrt(d)) / (2 * a);
-            Console.WriteLine($"x1 = {x1}\nx2 ={x2}");
+            Console.WriteLine($"There are 2 roots:\nx1 = {x1}\nx2 = {x2}");
         }
         else if (d == 0)
         {
             double x1;
             x1 = -b / (2 * a);
-            Console.WriteLine(x1);
+            Console.WriteLine($"There is 1 root:\n{x1}");
         }
         else
         {
-            Console.WriteLine("No roots");
+            Console.WriteLine("No roots found.");
+        }
+    }
+    static double inputCorrectNumber(string letter)
+    {
+        double i = 0;
+        Console.Write($"{letter}: ");
+        string test = Console.ReadLine();
+        if (double.TryParse(test, out i)) // tries if test can be parsed into double, if true - result goes to variable i
+        {
+            // i = Convert.ToDouble(test);
+            return i;
+        }
+        else
+        {
+            Console.WriteLine("Eror: Entered value isn't considered a number. Try again.");
+            return inputCorrectNumber(letter);
         }
     }
 }
