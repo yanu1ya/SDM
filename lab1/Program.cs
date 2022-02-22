773132719719e9741cc9b716f8732d0ce19bcd67
thank1ess <vladonyaroshchuk@gmail.com>
2022-02-22 09:57:58 +0200

    added non-interactive mode function and switch operator to choose the mode

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,16 +1,38 @@
 using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        interactiveMode();
+        Console.Write("Choose the mode (interactive: 0, non-interactive: 1): ");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "0":
+                interactiveMode();
+                break;
+            case "1":
+                nonInteractiveMode();
+                break;
+            default:
+                Console.WriteLine("*-*");
+                break;
+        }
     }
     static void interactiveMode()
     {
         double a, b, c;
         Console.WriteLine("You've chosen interactive mode.\nInput your numbers, please.");
         a = inputCorrectNumber("a");
+        if (a == 0) // exception
+        {
+            Console.WriteLine("Error: a can't be equal to 0. Try again.");
+            a = inputCorrectNumber("a");
+        }
 
         b = inputCorrectNumber("b");
 
@@ -19,6 +41,32 @@ class Program
         Console.WriteLine($"({a})x^2 + ({b})x + ({c}) = 0");
 
         findRoots(a, b, c);
+    }
+    static void nonInteractiveMode()
+    {
+        Console.Write("You've chosen non-interactive mode.\nInput file path here, please: ");
+        string path = Console.ReadLine();
+        if (File.Exists(path))
+        {
+            string result = File.ReadAllText(@path);
+            // path format has to be like this: "E:\storage\C#\1\coefs.txt"
+            string pattern = @"-?\d(\,\d*)? -?\d(\,\d*)? -?\d(\,\d*)?
+";
+            bool isMatch = Regex.IsMatch(result, pattern);
+            if (isMatch)
+            {
+                string[] numbers = result.Split(' ');
+                findRoots(Convert.ToDouble(numbers[0]), Convert.ToDouble(numbers[1]), Convert.ToDouble(numbers[2]));
+            }
+            else
+            {
+                Console.WriteLine("nop");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: Wrong path.");
+        }
     }
     static void findRoots(double a, double b, double c)
     {
@@ -49,11 +97,6 @@ class Program
         if (double.TryParse(test, out i)) // tries if test can be parsed into double, if true - result goes to variable i
         {
             // i = Convert.ToDouble(test);
-            if (letter == "a" && i == 0) // exception for a
-            {
-                Console.WriteLine("Error: a can't be equal to 0. Try again.");
-                return inputCorrectNumber("a");
-            }
             return i;
         }
         else
@@ -63,3 +106,4 @@ class Program
         }
     }
 }
+
